3dc1d67f33ecf60aef9f24efe5fae8bdb735040c
Poiuy7312 <pjs075637@gmail.com>
2025-10-13 09:51:40 -0400

    chore: put used colors in separate file

---
+++ src/colors.rs
@@ -0,0 +1,11 @@
+use sdl2::pixels::Color;
+
+pub const RED: Color = Color::RGB(255, 0, 0);
+pub const GREEN: Color = Color::RGB(0, 255, 0);
+pub const BLACK: Color = Color::RGB(0, 0, 0);
+pub const WHITE: Color = Color::RGB(255, 255, 255);
+pub const PRIMARY_COLOR: Color = Color::RGB(84, 84, 84);
+pub const SECONDARY_COLOR: Color = Color::RGB(150, 150, 150);
+pub const TERTIARY_COLOR: Color = Color::RGB(156, 156, 156);
+pub const QUATERNARY_COLOR: Color = Color::RGB(100, 100, 100);
+pub const HOVER_COLOR: Color = Color::RGB(200, 200, 200);
