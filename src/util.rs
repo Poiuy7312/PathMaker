60f41e3995de15685037a45edd769031634dcdf6
Poiuy7312 <pjs075637@gmail.com>
2025-10-16 13:39:53 -0400

    feat: added get_dir_map function

--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use sdl2::rect::{Point, Rect};
 
 use crate::{components::button::*, fileDialog};
@@ -48,4 +50,33 @@ pub fn walk_tree(
         }
     }
     return buttons;
+}
+
+pub fn get_dir_map(
+    node: &fileDialog::DirectoryNode,
+    width: u32,
+) -> HashMap<String, Vec<Box<dyn ValidDropdownOption>>> {
+    let mut buttons: Vec<Box<dyn ValidDropdownOption>> = Vec::new();
+    let mut map: HashMap<String, Vec<Box<dyn ValidDropdownOption>>> = HashMap::new();
+    if node.is_dir {
+        for child in &node.children {
+            if child.is_dir {
+                buttons.push(Box::new(StandardButton {
+                    height: 25,
+                    width: 200,
+                    location: Point::new(width as i32 - 200, 62),
+                    text_color: WHITE,
+                    background_color: QUATERNARY_COLOR,
+                    hover_color: SECONDARY_COLOR,
+                    text: child.name.to_string(),
+                    id: child.path.to_string_lossy().to_string(),
+                    active: false,
+                    filter: None,
+                }));
+                map.extend(get_dir_map(child, width));
+            }
+        }
+        map.insert(node.path.to_string_lossy().to_string(), buttons);
+    }
+    return map;
 }
