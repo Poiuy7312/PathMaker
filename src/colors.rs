f142348bfd05b0cba0b82ec1fbfcd2c31a3fdc4c
Poiuy7312 <pjs075637@gmail.com>
2026-01-26 10:26:15 -0500

    added: Yellow for new weighted node color

--- src/colors.rs
+++ src/colors.rs
@@ -4,6 +4,7 @@ pub const RED: Color = Color::RGB(255, 0, 0);
 pub const GREEN: Color = Color::RGB(0, 255, 0);
 pub const BLACK: Color = Color::RGB(0, 0, 0);
 pub const WHITE: Color = Color::RGB(255, 255, 255);
+pub const YELLOW: Color = Color::RGB(255, 255, 0);
 pub const PRIMARY_COLOR: Color = Color::RGB(84, 84, 84);
 pub const SECONDARY_COLOR: Color = Color::RGB(150, 150, 150);
 pub const TERTIARY_COLOR: Color = Color::RGB(156, 156, 156);
